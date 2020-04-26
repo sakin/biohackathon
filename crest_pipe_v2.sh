1a18b7b914b4a377d16dba8e8d45e5a73fe8f102
Ubuntu <ubuntu@ip-172-31-23-114.us-west-2.compute.internal>
2020-04-26 19:13:22 +0000

    Uploading shell scripts

---
+++ crest_pipe_v2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+#seed_sum= zless $1
+
+seed_num=$(zless $1 | awk '$2 !="NA"' | awk '{sub(/\//,"\t",$2)};1' | sed 's/[rs"]//g' | awk 'FNR > 1 {if ($2 == "A") x=1; if ($2 == "C") x=2; if ($2 == "T") x=3; if ($2 == "G") x=4; if ($3 == "A") y=1; if ($3 == "C") y=2; if ($3 == "T") y=3; if ($3 == "G") y=4; z=$1; u=y+x; s=1/u; q=z^s; sum += q} END {print sum}')
+
+printf "\nThis is the generated seed num: ${seed_num}\n\n"
+printf [[ $- == *i* ]] && echo 'Interactive' || echo 'Not interactive'
+
+#exit
+#cd /home/ubuntu/poetry/poetry-generator
+#gen_poem=$(sudo python /home/ubuntu/poetry/poetry-generator/poem_v2.py ${seed_num}) 
+#leave me commented out sudo python /home/ubuntu/poetry/poetry-generator/poem_v2.py ${seed_num} > generated_poem.txt
+#cd /home/ubuntu/Jons_working_e
+
+image_name=$2
+
+export CUDNN_PATH=/usr/local/cuda/lib64/libcudnn.so.7.6.5
+
+cd /home/ubuntu/dcgan.torch
+/home/ubuntu/dcgan.torch/generate_crest_seeded.sh ${image_name} ${seed_num}
+cd /home/ubuntu/Jons_working_env
+
+
+#mv /home/ubuntu/dcgan.torch/${image_name}.png /home/ubuntu/Jons_working_env/${image_name}.png
+
+screen -d ${image_name}
